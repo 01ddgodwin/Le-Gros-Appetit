d9b18e3c9b3b0c60cec77bad95d2bf507a802403
Dillon Godwin <ddgodwin01@byui.edu>
2022-03-17 10:05:04 -0600

    Updated

--- app.js
+++ app.js
@@ -19,11 +19,10 @@ app.use((req, res, next) => {
 app.use('/feed', feedRoutes);
 
 const PORT = process.env.PORT || 8080;
-mongoose.connect('mongodb+srv://dillon:bob112172@le-gros-appetit.xibg7.mongodb.net/test?authSource=admin&replicaSet=atlas-qwuacs-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
-)
-.then(result => {
+//mongoose.connect('mongodb+srv://dillon:bob112172@le-gros-appetit.xibg7.mongodb.net/test?authSource=admin&replicaSet=atlas-qwuacs-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+//.then(result => {
   app.listen(PORT, () => {
     console.warn(`App listening on http://localhost:${PORT}`);
   });
-})
-.catch(err => console.log(err));
+//})
+//.catch(err => console.log(err));
